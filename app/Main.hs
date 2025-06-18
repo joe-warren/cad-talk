b9dbb161bca46eca149ac22cf1d21bbafcced94a
Joseph Warren <hungryjoe.warren@gmail.com>
2025-06-18 23:31:54 +0100

    support hot-reloading

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,9 @@ import qualified Data.Text as T
 import qualified Data.Map as M
 import Text.DocTemplates
 import Timeline (addTimeline)
+import qualified System.FSNotify as FSNotify
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 
 parseInput :: Text -> IO Pandoc
 parseInput txt =
@@ -30,12 +33,22 @@ printSlides doc template dzcore =
     in runIOorExplode $ 
             writeDZSlides writerOptions doc
 
-main :: IO ()
-main = do 
+rebuild :: IO ()
+rebuild = do 
+    putStrLn "rebuilding"
     doc <- addTimeline =<< parseInput =<< T.readFile "Presentation.md"
     templateTxt <- T.readFile "template.html" 
     dzcoreTxt <- T.readFile "dz-core.html" 
     template <- either error id <$> compileTemplate "." templateTxt
     T.writeFile "slides.html" =<< printSlides doc template dzcoreTxt
     putStrLn "done"
+
+main :: IO ()
+main = do
+    rebuild
+    FSNotify.withManager $ \mgr -> do
+        FSNotify.watchDir mgr "Presentation.md" (const True) (const rebuild)
+        -- sleep until interrupted
+        forever $ threadDelay 1000000
+    
 
